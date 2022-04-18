0f3198d4ed273c61c64391652b6e16f46602d1d3
tejasfirodiya <tejas.firodiya@waiin.com>
2022-04-18 18:14:33 +0530

    angular practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Comp1 } from './newComp/new1';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1
   ],
   imports: [
     BrowserModule
---
+++ src/app/newComp/new1.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "newSelector",
+    templateUrl : './new1.html'
+})
+
+export class Comp1
+{
+    name = "Tejas";
+    age:number = 22;
+}
